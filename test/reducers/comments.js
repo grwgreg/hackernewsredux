e83594d9a4ad3f83ee9b5ab9c71bf6abb20f642d
Greg Weisman <grwgreg@gmail.com>
2016-03-16 22:08:59 -0400

    add tests for comments and news reducers

---
+++ test/reducers/comments.js
@@ -0,0 +1,90 @@
+import { expect } from 'chai';
+
+import {comments as reducer} from '../../src/reducers/comments'
+import c from '../../src/constants'
+
+//http://chaijs.com/api/bdd/
+//http://redux.js.org/docs/recipes/WritingTests.html
+
+
+describe('Comments reducers', ()=> {
+  it('should return initial state', ()=> {
+
+    const initialState = {
+      loading: false,
+      items: {},
+      currentId: undefined
+    }
+
+    expect(reducer(undefined, {}))
+      .to.deep.equal(initialState)
+
+  })
+
+  it('should handle LOAD_COMMENTS_START', ()=> {
+
+    const state = {
+      loading: false,
+      items: {},
+      currentId: undefined
+    }
+    const newState = {
+      loading: true,
+      items: {},
+      currentId: undefined
+    }
+
+    expect(reducer(state, { type: c.LOAD_COMMENTS_START }))
+      .to.deep.equal(newState)
+
+  })
+
+  it('should handle SET_COMMENTS_CURRENT_ID', ()=> {
+
+    const state = {
+      loading: true,
+      items: {},
+      currentId: undefined
+    }
+    const newState = {
+      loading: false,
+      items: {},
+      currentId: 44
+    }
+
+    expect(reducer(state, {
+      type: c.SET_COMMENTS_CURRENT_ID,
+      payload: {
+        id: 44
+      }
+     }))
+      .to.deep.equal(newState)
+
+  })
+
+  it('should handle LOAD_COMMENTS_SUCCESS', ()=> {
+
+    const state = {
+      loading: true,
+      items: {44: {id:44}},
+      currentId: 44
+    }
+    const newState = {
+      loading: false,
+      items: {
+        44: {id:44},
+        55: {id:55}
+      },
+      currentId: 55
+    }
+
+    expect(reducer(state, {
+      type: c.LOAD_COMMENTS_SUCCESS,
+      payload: {
+        comments: {id:55}
+      }
+     }))
+      .to.deep.equal(newState)
+
+  })
+})
---
+++ test/reducers/news.js
@@ -0,0 +1,162 @@
+import { expect } from 'chai';
+
+import reducers from '../../src/reducers/news'
+import c from '../../src/constants'
+
+//http://chaijs.com/api/bdd/
+//http://redux.js.org/docs/recipes/WritingTests.html
+
+const newsTypes = [
+    c.TOP_STORIES,
+    c.SHOW_STORIES,
+    c.ASK_STORIES,
+    c.JOB_STORIES,
+    c.NEW_STORIES
+  ]
+
+describe('News reducers', ()=> {
+  it('should return initial state', ()=> {
+
+    const initialState = {
+      loading: false,
+      currentlyDisplaying: 0,
+      loadableItems: [],
+      items: []
+    }
+
+    newsTypes.forEach(type => {
+      expect(reducers[type](undefined, {}))
+        .to.deep.equal(initialState)
+    })
+
+  })
+  it('should handle LOAD_NEWS_START', ()=> {
+
+    const state = {
+      loading: false,
+      currentlyDisplaying: 3,
+      loadableItems: [1,2,3,4,5,6],
+      items: [{id:1},{id:2},{id:3}]
+    }
+    const newState = {
+      loading: true,
+      currentlyDisplaying: 3,
+      loadableItems: [1,2,3,4,5,6],
+      items: [{id:1},{id:2},{id:3}]
+    }
+    const newStateInitialLoad = {
+      loading: true,
+      currentlyDisplaying: 0,
+      loadableItems: [1,2,3,4,5,6],
+      items: [{id:1},{id:2},{id:3}]
+    }
+
+    newsTypes.forEach(type => {
+      expect(reducers[type](state, {
+        type: c.LOAD_NEWS_START,
+        payload: {
+          initialLoad: false,
+          newsType: type
+        }
+      }))
+        .to.deep.equal(newState)
+
+      expect(reducers[type](state, {
+        type: c.LOAD_NEWS_START,
+        payload: {
+          initialLoad: true,
+          newsType: type
+        }
+      }))
+        .to.deep.equal(newStateInitialLoad)
+    })
+
+  })
+
+  it('should handle LOAD_NEWS_SUCCESS', ()=> {
+
+    const state = {
+      loading: true,
+      currentlyDisplaying: 3,
+      loadableItems: [1,2,3,4,5,6],
+      items: [{id:1},{id:2},{id:3}]
+    }
+    const newState = {
+      loading: false,
+      currentlyDisplaying: 6,
+      loadableItems: [1,2,3,4,5,6],
+      items: [{id:1},{id:2},{id:3},{id:4},{id:5},{id:6}]
+    }
+
+    newsTypes.forEach(type => {
+      expect(reducers[type](state, {
+        type: c.LOAD_NEWS_SUCCESS,
+        payload: {
+          data: [{id:4},{id:5},{id:6}],
+          newsType: type
+        }
+      }))
+        .to.deep.equal(newState)
+
+    })
+
+  })
+
+  it('should handle LOAD_NEWS_INCREMENT_DISPLAYING', ()=> {
+
+    const state = {
+      loading: true,
+      currentlyDisplaying: 0,
+      loadableItems: [1,2,3,4,5,6],
+      items: [{id:1},{id:2},{id:3}]
+    }
+    const newState = {
+      loading: false,
+      currentlyDisplaying: 3,
+      loadableItems: [1,2,3,4,5,6],
+      items: [{id:1},{id:2},{id:3}]
+    }
+
+    newsTypes.forEach(type => {
+      expect(reducers[type](state, {
+        type: c.LOAD_NEWS_INCREMENT_DISPLAYING,
+        payload: {
+          count: 3,
+          newsType: type
+        }
+      }))
+        .to.deep.equal(newState)
+
+    })
+
+  })
+
+  it('should handle LOAD_NEWS_INCREMENT_DISPLAYING', ()=> {
+
+    const state = {
+      loading: true,
+      currentlyDisplaying: 0,
+      loadableItems: [],
+      items: []
+    }
+    const newState = {
+      loading: true,
+      currentlyDisplaying: 0,
+      loadableItems: [1,2,3,4,5,6],
+      items: []
+    }
+
+    newsTypes.forEach(type => {
+      expect(reducers[type](state, {
+        type: c.SET_NEWS_LOADABLE_ITEMS,
+        payload: {
+          items: [1,2,3,4,5,6],
+          newsType: type
+        }
+      }))
+        .to.deep.equal(newState)
+
+    })
+
+  })
+})
